063a73ed382143c790f6a69e6871cd072c95048d
WALF <ce.walf@gmail.com>
2024-07-25 20:49:08 +0900

    Initial commit

---
+++ src/main/java/com/chillvibe/chillvibe/ChillvibeApplication.java
@@ -0,0 +1,13 @@
+package com.chillvibe.chillvibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillvibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillvibeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chillvibe/chillvibe/ChillvibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chillvibe.chillvibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillvibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
