bcc448d5e0e59c005244502e318d7a197bb769ed
[Cloud3] 이용경 <ce.walf@gmail.com>
2024-07-30 00:11:00 +0900

    feat: PlaylistTrackController 추가

--- src/main/java/com/chillvibe/chillvibe/domain/playlist/controller/PlaylistTrackController.java
+++ src/main/java/com/chillvibe/chillvibe/domain/playlist/controller/PlaylistTrackController.java
@@ -1,5 +1,15 @@
 package com.chillvibe.chillvibe.domain.playlist.controller;
 
-public class PlaylistTrackController {
+import com.chillvibe.chillvibe.domain.playlist.service.PlaylistTrackService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/playlist")
+public class PlaylistTrackController {
+  private final PlaylistTrackService playlistTrackService;
+
+  public PlaylistTrackController (PlaylistTrackService playlistTrackService){
+    this.playlistTrackService = playlistTrackService;
+  }
 }
