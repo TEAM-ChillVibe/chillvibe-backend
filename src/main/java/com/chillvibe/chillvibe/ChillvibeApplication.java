0efc6b722a15d77f25d79904e7167c8bde6e7ece
[Cloud3] 이용경 <ce.walf@gmail.com>
2024-07-29 13:38:40 +0900

    chore: 프로젝트 기본 설정 추가

--- src/main/java/com/chillvibe/chillvibe/ChillvibeApplication.java
+++ src/main/java/com/chillvibe/chillvibe/ChillvibeApplication.java
@@ -3,12 +3,14 @@ package com.chillvibe.chillvibe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 // (exclude = {SecurityAutoConfiguration.class})
 // 이 부분 설정하지 않으면 api 요청이 안됩니다!
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableScheduling
+@EnableJpaAuditing
 public class ChillvibeApplication {
 
   public static void main(String[] args) {
