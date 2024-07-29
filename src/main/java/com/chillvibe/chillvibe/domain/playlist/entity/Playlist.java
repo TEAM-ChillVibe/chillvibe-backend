b11aa0638d470863221b85d026081409045c7efc
[Cloud3] 이용경 <ce.walf@gmail.com>
2024-07-29 23:16:55 +0900

    feat: Playlist, PlaylistTrack 엔티티 작성

--- src/main/java/com/chillvibe/chillvibe/domain/playlist/entity/Playlist.java
+++ src/main/java/com/chillvibe/chillvibe/domain/playlist/entity/Playlist.java
@@ -1,5 +1,38 @@
 package com.chillvibe.chillvibe.domain.playlist.entity;
 
+import com.chillvibe.chillvibe.domain.user.entity.User;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "Playlist")
 public class Playlist {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(name = "image_url", nullable = false)
+  private String imageUrl;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  // Playlist가 삭제되면, 연관된 모든 PlaylistTrack 엔티티들도 삭제
+  @OneToMany(mappedBy = "playlist", cascade =  CascadeType.ALL, orphanRemoval = true)
+  private List<PlaylistTrack> tracks;
 
 }
--- src/main/java/com/chillvibe/chillvibe/domain/playlist/entity/PlaylistTrack.java
+++ src/main/java/com/chillvibe/chillvibe/domain/playlist/entity/PlaylistTrack.java
@@ -1,5 +1,37 @@
 package com.chillvibe.chillvibe.domain.playlist.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "PlaylistTrack")
 public class PlaylistTrack {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @ManyToOne
+  @JoinColumn(name = "playlist_id", nullable = false)
+  private Playlist playlist;
+
+  @Column(name = "track_id", nullable = false)
+  private String trackId;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "artist", nullable = false)
+  private String artist;
+
+  @Column(name = "duration", nullable = false)
+  private String duration;
+
+  @Column(name = "preview_url")
+  private String previewUrl;
+
+  @Column(name = "thumbnail_url", nullable = false)
+  private String thumbnailUrl;
 
 }
