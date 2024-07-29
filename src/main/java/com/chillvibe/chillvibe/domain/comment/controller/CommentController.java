bc0cbc1071f6cee1e2ec9b494f689542c0296038
이소정 <sojeong@isojeong-ui-MacBookAir.local>
2024-07-30 01:37:47 +0900

    feat: comment 수정, 삭제

--- src/main/java/com/chillvibe/chillvibe/domain/comment/controller/CommentController.java
+++ src/main/java/com/chillvibe/chillvibe/domain/comment/controller/CommentController.java
@@ -8,8 +8,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,30 +30,59 @@ public class CommentController {
   }
 
   @GetMapping
-  public ResponseEntity<List<CommentResponseDto>> getComments(@RequestParam Long postId,
-      @RequestParam Long userId) {
-    List<CommentResponseDto> comments;
+  public ResponseEntity<List<CommentResponseDto>> getCommentsByPost(@RequestParam Long postId) {
+    List<CommentResponseDto> comments = commentService.getCommentsByPost(postId);
+    return new ResponseEntity<>(comments, HttpStatus.OK);
+  }
 
-    if (postId != null) {
-      comments = commentService.getCommentsByPost(postId);
-    } else if (userId != null) {
-      comments = commentService.getCommentsByUser(userId);
-    } else {
-      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
+  @GetMapping("/user")
+  public ResponseEntity<List<CommentResponseDto>> getComments(@RequestParam Long userId) {
+    List<CommentResponseDto> comments = commentService.getCommentsByUser(userId);
     return new ResponseEntity<>(comments, HttpStatus.OK);
   }
 
   @PostMapping
-  public ResponseEntity<CommentResponseDto> createComment(
-      @RequestBody CommentRequestDto requestDto, Principal principal) {
+  public ResponseEntity<CommentResponseDto> createComment(@RequestBody CommentRequestDto requestDto,
+      Principal principal) {
     if (principal == null) {
       return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
     String email = principal.getName();
-    CommentResponseDto responseDto = commentService.create(requestDto, email);
+    CommentResponseDto responseDto = commentService.createComment(requestDto, email);
     return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
   }
 
+  @PutMapping("/{commentId}")
+  public ResponseEntity<CommentResponseDto> updateComment(@PathVariable Long commentId,
+      CommentRequestDto requestDto, Principal principal) {
+    if (commentId == null) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 
+    if (principal == null) {
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+    String email = principal.getName();
+    CommentResponseDto responseDto = commentService.updateComment(commentId, requestDto, email);
+
+    return new ResponseEntity<>(responseDto, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{commentId}")
+  public ResponseEntity<CommentResponseDto> deleteComment(@PathVariable Long commentId,
+      Principal principal) {
+    if (commentId == null) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    if (principal == null) {
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+    String email = principal.getName();
+    commentService.deleteComment(commentId, email);
+
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
 }
