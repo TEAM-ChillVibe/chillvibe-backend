16c33832704d7b7c4f62f34b0140742998618bcd
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-08-01 14:47:39 +0900

    chore: post entity PK 컬럼 어노테이션 삭제

--- src/main/java/com/chillvibe/chillvibe/domain/post/entity/Post.java
+++ src/main/java/com/chillvibe/chillvibe/domain/post/entity/Post.java
@@ -7,24 +7,18 @@ import com.chillvibe.chillvibe.domain.user.entity.User;
 import com.chillvibe.chillvibe.global.common.BaseTimeEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,7 +35,6 @@ public class Post extends BaseTimeEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "post_id")
   private Long id;
 
   @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
