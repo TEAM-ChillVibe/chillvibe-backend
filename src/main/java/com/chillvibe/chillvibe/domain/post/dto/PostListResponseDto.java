40157c2fd269a8f348327b24505fe09926ad02a3
[Cloud3] 안지완 <an_7375@daum.net>
2024-08-02 22:58:44 +0900

    merge

--- src/main/java/com/chillvibe/chillvibe/domain/post/dto/PostListResponseDto.java
+++ src/main/java/com/chillvibe/chillvibe/domain/post/dto/PostListResponseDto.java
@@ -1,5 +1,37 @@
 package com.chillvibe.chillvibe.domain.post.dto;
 
+import com.chillvibe.chillvibe.domain.post.entity.Post;
+import com.chillvibe.chillvibe.domain.user.dto.UserSimpleResponseDto;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class PostListResponseDto {
 
+  private Long id;
+  private String title;
+  private LocalDateTime createdAt;
+  private Integer trackCount;
+  private List<String> hashtags;
+  private UserSimpleResponseDto user;
+  private Integer likeCount;
+
+  public PostListResponseDto(Post post) {
+    this.id = post.getId();
+    this.title = post.getTitle();
+    this.createdAt = post.getCreatedAt();
+    this.trackCount = post.getPlaylist() != null ? post.getPlaylist().getTracks().size() : 0;
+    this.hashtags = post.getPostHashtag().stream()
+        .map(postHashtag -> postHashtag.getHashtag().getName())
+        .toList();
+    this.user = post.getUser() != null ? new UserSimpleResponseDto(
+        post.getUser().getId(),
+        post.getUser().getNickname(),
+        post.getUser().getProfileUrl()
+    ) : null;
+    this.likeCount = post.getLikeCount() != null ? post.getLikeCount() : 0;
+  }
 }
