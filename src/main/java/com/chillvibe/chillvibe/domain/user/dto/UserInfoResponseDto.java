bf8e4e552dcf520f0a09e680164a76ceda9d65e0
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-08-03 17:38:17 +0900

    fix: user get 요청 시 hashtag 중첩문제 해결

--- src/main/java/com/chillvibe/chillvibe/domain/user/dto/UserInfoResponseDto.java
+++ src/main/java/com/chillvibe/chillvibe/domain/user/dto/UserInfoResponseDto.java
@@ -1,12 +1,10 @@
 package com.chillvibe.chillvibe.domain.user.dto;
 
+import com.chillvibe.chillvibe.domain.hashtag.dto.HashtagResponseDto;
 import com.chillvibe.chillvibe.domain.hashtag.entity.Hashtag;
-import com.chillvibe.chillvibe.domain.post.entity.Post;
 import com.chillvibe.chillvibe.domain.user.entity.User;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,7 +19,7 @@ public class UserInfoResponseDto {
   private String profileUrl;
   private String introduction;
   private boolean isPublic;
-  private List<Hashtag> tagLists;
+  private List<HashtagResponseDto> tagLists;
 
   public UserInfoResponseDto(User user, List<Hashtag> hashtags) {
     this.userId = user.getId();
@@ -30,6 +28,8 @@ public class UserInfoResponseDto {
     this.profileUrl = user.getProfileUrl();
     this.introduction = user.getIntroduction();
     this.isPublic = user.isPublic();
-    this.tagLists = hashtags;
+    this.tagLists = hashtags.stream()
+        .map(hashtag -> new HashtagResponseDto(hashtag.getId(), hashtag.getName(),
+            hashtag.getTotalLikes())).toList();
   }
 }
