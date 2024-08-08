1324c8201b6ab32b75346d7e5b1c16b7ad6757d7
이소정 <sojeong@isojeong-ui-MacBookAir.local>
2024-08-08 21:32:58 +0900

    commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -54,6 +54,13 @@ VALUES (1, 1),
 -- playlist
 INSERT INTO playlist (title, image_url, user_id)
 VALUES ('Chill Vibes', 'http://example.com/image1', 1);
+
+-- INSERT INTO playlist (title, image_url, user_id)
+-- VALUES ('Chill Vibes2', 'http://example.com/image2', 1);
+--
+-- INSERT INTO playlist (title, image_url, user_id)
+-- VALUES ('Chill Vibes3', 'http://example.com/image2', 1);
+
 INSERT INTO playlist (title, image_url, user_id)
 VALUES ('Workout Hits', 'http://example.com/image2', 2);
 INSERT INTO playlist (title, image_url, user_id)
@@ -67,55 +74,90 @@ VALUES ('Study Beats', 'http://example.com/image5', 5);
 -- PlaylistTrack
 INSERT INTO playlist_track (playlist_id, track_id, name, artist, duration, preview_url,
                             thumbnail_url)
-VALUES (1, '0qsv5I5fEnRoX2Enb3mBNr', 'V (Peace) (feat. AKMU)', 'Zion.T', '02:28','https://p.scdn.co/mp3-preview/0518203f58eee6b20829769f51aa11e6ece5bfa9?cid=85cc36f6ed374905aebe0d7e5ea27763', 'https://i.scdn.co/image/ab67616d0000b273df146c9e8246ae9f1d7711da'),
-       (1, '2Zr0bYRHwWXi7eM2wuE6Aj', 'VVS (Feat. JUSTHIS) (Prod. GroovyRoom)', 'MIRANI', '05:35', 'https://p.scdn.co/mp3-preview/6ae395b57eb158b6d1317083ee2785a4ceae1c3d?cid=85cc36f6ed374905aebe0d7e5ea27763', 'https://i.scdn.co/image/ab67616d0000b2734fc43bf4163c96eca2c640d8'),
+VALUES (1, '0qsv5I5fEnRoX2Enb3mBNr', 'V (Peace) (feat. AKMU)', 'Zion.T', '02:28',
+        'https://p.scdn.co/mp3-preview/0518203f58eee6b20829769f51aa11e6ece5bfa9?cid=85cc36f6ed374905aebe0d7e5ea27763',
+        'https://i.scdn.co/image/ab67616d0000b273df146c9e8246ae9f1d7711da'),
+       (1, '2Zr0bYRHwWXi7eM2wuE6Aj', 'VVS (Feat. JUSTHIS) (Prod. GroovyRoom)', 'MIRANI', '05:35',
+        'https://p.scdn.co/mp3-preview/6ae395b57eb158b6d1317083ee2785a4ceae1c3d?cid=85cc36f6ed374905aebe0d7e5ea27763',
+        'https://i.scdn.co/image/ab67616d0000b2734fc43bf4163c96eca2c640d8'),
 
-       (1, '06uScjJGabF4rIRdejWOaL', 'Congratulations', 'DAY6', '02:45', 'https://p.scdn.co/mp3-preview/2712bb4423e59ca467b8b628c42d3eef91e0b55d?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (1, '06uScjJGabF4rIRdejWOaL', 'Congratulations', 'DAY6', '02:45',
+        'https://p.scdn.co/mp3-preview/2712bb4423e59ca467b8b628c42d3eef91e0b55d?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b2739989437bcd5115edde791ac8'),
 
-       (1, '3pDGJRDN3p6kCNZcD97FYY4', 'Cosmic', 'Red Velvet', '03:45', 'https://p.scdn.co/mp3-preview/4391208647f7f1f5362e51143eacaee853417fe6?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (1, '3pDGJRDN3p6kCNZcD97FYY4', 'Cosmic', 'Red Velvet', '03:45',
+        'https://p.scdn.co/mp3-preview/4391208647f7f1f5362e51143eacaee853417fe6?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b273a54b93bff4827437f56b7896'),
-       (1, '02wk5BttM0QL38ERjLPQJB', 'Cool With You', 'NewJeans', '02:27', 'https://p.scdn.co/mp3-preview/598e05a7fd20278aca5477e1993b252e8fc97daf?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (1, '02wk5BttM0QL38ERjLPQJB', 'Cool With You', 'NewJeans', '02:27',
+        'https://p.scdn.co/mp3-preview/598e05a7fd20278aca5477e1993b252e8fc97daf?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b2730744690248ef3ba7b776ea7b'),
-       (1, '2DwUdMJ5uxv20EhAildreg', 'Cookie', 'NewJeans', '03:55', 'https://p.scdn.co/mp3-preview/d059b8ea163242692099c452cbed3de596c2e624?cid=85cc36f6ed374905aebe0d7e5ea27763', 'https://i.scdn.co/image/ab67616d0000b2739d28fd01859073a3ae6ea209'),
+       (1, '2DwUdMJ5uxv20EhAildreg', 'Cookie', 'NewJeans', '03:55',
+        'https://p.scdn.co/mp3-preview/d059b8ea163242692099c452cbed3de596c2e624?cid=85cc36f6ed374905aebe0d7e5ea27763',
+        'https://i.scdn.co/image/ab67616d0000b2739d28fd01859073a3ae6ea209'),
 
-       (2, '72cq3rZCIEYaq1TM8y5LBQ', 'Your Dog Loves You (Feat. Crush)', 'Colde', '04:33', 'https://p.scdn.co/mp3-preview/591687ac1da4e48c3069043664cde3e6bd4ab1b9?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (2, '72cq3rZCIEYaq1TM8y5LBQ', 'Your Dog Loves You (Feat. Crush)', 'Colde', '04:33',
+        'https://p.scdn.co/mp3-preview/591687ac1da4e48c3069043664cde3e6bd4ab1b9?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b273b8b1020d37e55d5db307a0a7'),
-       (2, '57mWHxgHxZnhFhFiBrpYUV', 'Moon, 12:04am', 'offonoff', '03:43', 'https://p.scdn.co/mp3-preview/a61371ab2e3a918bb044959a485afab9cf3b999e?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (2, '57mWHxgHxZnhFhFiBrpYUV', 'Moon, 12:04am', 'offonoff', '03:43',
+        'https://p.scdn.co/mp3-preview/a61371ab2e3a918bb044959a485afab9cf3b999e?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b273d8ab8d3ddd6b3f1dff0ffb88'),
-       (2, '6hJ5fh34Zrreygumi7LMAN', '농담 반 진담 반', 'Sugarbowl', '03:23', 'https://p.scdn.co/mp3-preview/6d625f2240bd8d82421a0c0a6a96dab3c7713be4?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (2, '6hJ5fh34Zrreygumi7LMAN', '농담 반 진담 반', 'Sugarbowl', '03:23',
+        'https://p.scdn.co/mp3-preview/6d625f2240bd8d82421a0c0a6a96dab3c7713be4?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b27303bb541f71bd3a8d30a37da0'),
-       (2, '1l3qnaenScIWpAyJRbjLmP', 'PUZZLE', 'Sugarbowl', '03:31', 'https://p.scdn.co/mp3-preview/efe2a61023c9cebe056b51c76a28b331d864da33?cid=85cc36f6ed374905aebe0d7e5ea27763', 'https://i.scdn.co/image/ab67616d0000b2731ad157781a18ed1c8e832670'),
+       (2, '1l3qnaenScIWpAyJRbjLmP', 'PUZZLE', 'Sugarbowl', '03:31',
+        'https://p.scdn.co/mp3-preview/efe2a61023c9cebe056b51c76a28b331d864da33?cid=85cc36f6ed374905aebe0d7e5ea27763',
+        'https://i.scdn.co/image/ab67616d0000b2731ad157781a18ed1c8e832670'),
 
-       (3, '72p6J6txwmawWVC9Gc9vwg', '오늘밤', 'Sugarbowl', '3:30', 'https://p.scdn.co/mp3-preview/5c23ba90529f246c9137299da5bce2dde89a834a?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (3, '72p6J6txwmawWVC9Gc9vwg', '오늘밤', 'Sugarbowl', '3:30',
+        'https://p.scdn.co/mp3-preview/5c23ba90529f246c9137299da5bce2dde89a834a?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b2736610b1cd859feae3598e7d22'),
-       (3, '33xRp6ZX1DKraRFHR9ZDck', 'Forest', 'Choi Yu Ree', '03:48', 'https://p.scdn.co/mp3-preview/cfecfbfac21935ed9ff395b1cc12debad3b2a50d?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (3, '33xRp6ZX1DKraRFHR9ZDck', 'Forest', 'Choi Yu Ree', '03:48',
+        'https://p.scdn.co/mp3-preview/cfecfbfac21935ed9ff395b1cc12debad3b2a50d?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b2736012b2482accbd98980ad1c5'),
-       (3, '12n9WBfLII5EdQFzQiQFqv', '"Wish', 'Choi Yu Ree', '03:57', 'https://p.scdn.co/mp3-preview/23440e95aea744bc060c0badc4e493d322f9ef4f?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (3, '12n9WBfLII5EdQFzQiQFqv', '"Wish', 'Choi Yu Ree', '03:57',
+        'https://p.scdn.co/mp3-preview/23440e95aea744bc060c0badc4e493d322f9ef4f?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b27316f90eca8734936f3f8ef7dc'),
-       (3, '0qsv5I5fEnRoX2Enb3mBNr', 'V (Peace) (feat. AKMU)', 'Zion.T', '02:28','https://p.scdn.co/mp3-preview/0518203f58eee6b20829769f51aa11e6ece5bfa9?cid=85cc36f6ed374905aebe0d7e5ea27763', 'https://i.scdn.co/image/ab67616d0000b273df146c9e8246ae9f1d7711da'),
-       (3, '2Zr0bYRHwWXi7eM2wuE6Aj', 'VVS (Feat. JUSTHIS) (Prod. GroovyRoom)', 'MIRANI', '05:35', 'https://p.scdn.co/mp3-preview/6ae395b57eb158b6d1317083ee2785a4ceae1c3d?cid=85cc36f6ed374905aebe0d7e5ea27763', 'https://i.scdn.co/image/ab67616d0000b2734fc43bf4163c96eca2c640d8'),
+       (3, '0qsv5I5fEnRoX2Enb3mBNr', 'V (Peace) (feat. AKMU)', 'Zion.T', '02:28',
+        'https://p.scdn.co/mp3-preview/0518203f58eee6b20829769f51aa11e6ece5bfa9?cid=85cc36f6ed374905aebe0d7e5ea27763',
+        'https://i.scdn.co/image/ab67616d0000b273df146c9e8246ae9f1d7711da'),
+       (3, '2Zr0bYRHwWXi7eM2wuE6Aj', 'VVS (Feat. JUSTHIS) (Prod. GroovyRoom)', 'MIRANI', '05:35',
+        'https://p.scdn.co/mp3-preview/6ae395b57eb158b6d1317083ee2785a4ceae1c3d?cid=85cc36f6ed374905aebe0d7e5ea27763',
+        'https://i.scdn.co/image/ab67616d0000b2734fc43bf4163c96eca2c640d8'),
 
-       (3, '06uScjJGabF4rIRdejWOaL', 'Congratulations', 'DAY6', '02:45', 'https://p.scdn.co/mp3-preview/2712bb4423e59ca467b8b628c42d3eef91e0b55d?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (3, '06uScjJGabF4rIRdejWOaL', 'Congratulations', 'DAY6', '02:45',
+        'https://p.scdn.co/mp3-preview/2712bb4423e59ca467b8b628c42d3eef91e0b55d?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b2739989437bcd5115edde791ac8'),
 
-       (4, '3pDGJRDN3p6kCNZcD97FYY4', 'Cosmic', 'Red Velvet', '03:45', 'https://p.scdn.co/mp3-preview/4391208647f7f1f5362e51143eacaee853417fe6?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (4, '3pDGJRDN3p6kCNZcD97FYY4', 'Cosmic', 'Red Velvet', '03:45',
+        'https://p.scdn.co/mp3-preview/4391208647f7f1f5362e51143eacaee853417fe6?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b273a54b93bff4827437f56b7896'),
-       (4, '02wk5BttM0QL38ERjLPQJB', 'Cool With You', 'NewJeans', '02:27', 'https://p.scdn.co/mp3-preview/598e05a7fd20278aca5477e1993b252e8fc97daf?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (4, '02wk5BttM0QL38ERjLPQJB', 'Cool With You', 'NewJeans', '02:27',
+        'https://p.scdn.co/mp3-preview/598e05a7fd20278aca5477e1993b252e8fc97daf?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b2730744690248ef3ba7b776ea7b'),
-       (4, '2DwUdMJ5uxv20EhAildreg', 'Cookie', 'NewJeans', '03:55', 'https://p.scdn.co/mp3-preview/d059b8ea163242692099c452cbed3de596c2e624?cid=85cc36f6ed374905aebe0d7e5ea27763', 'https://i.scdn.co/image/ab67616d0000b2739d28fd01859073a3ae6ea209'),
+       (4, '2DwUdMJ5uxv20EhAildreg', 'Cookie', 'NewJeans', '03:55',
+        'https://p.scdn.co/mp3-preview/d059b8ea163242692099c452cbed3de596c2e624?cid=85cc36f6ed374905aebe0d7e5ea27763',
+        'https://i.scdn.co/image/ab67616d0000b2739d28fd01859073a3ae6ea209'),
 
-       (4, '72cq3rZCIEYaq1TM8y5LBQ', 'Your Dog Loves You (Feat. Crush)', 'Colde', '04:33', 'https://p.scdn.co/mp3-preview/591687ac1da4e48c3069043664cde3e6bd4ab1b9?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (4, '72cq3rZCIEYaq1TM8y5LBQ', 'Your Dog Loves You (Feat. Crush)', 'Colde', '04:33',
+        'https://p.scdn.co/mp3-preview/591687ac1da4e48c3069043664cde3e6bd4ab1b9?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b273b8b1020d37e55d5db307a0a7'),
-       (5, '57mWHxgHxZnhFhFiBrpYUV', 'Moon, 12:04am', 'offonoff', '03:43', 'https://p.scdn.co/mp3-preview/a61371ab2e3a918bb044959a485afab9cf3b999e?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (5, '57mWHxgHxZnhFhFiBrpYUV', 'Moon, 12:04am', 'offonoff', '03:43',
+        'https://p.scdn.co/mp3-preview/a61371ab2e3a918bb044959a485afab9cf3b999e?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b273d8ab8d3ddd6b3f1dff0ffb88'),
-       (5, '6hJ5fh34Zrreygumi7LMAN', '농담 반 진담 반', 'Sugarbowl', '03:23', 'https://p.scdn.co/mp3-preview/6d625f2240bd8d82421a0c0a6a96dab3c7713be4?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (5, '6hJ5fh34Zrreygumi7LMAN', '농담 반 진담 반', 'Sugarbowl', '03:23',
+        'https://p.scdn.co/mp3-preview/6d625f2240bd8d82421a0c0a6a96dab3c7713be4?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b27303bb541f71bd3a8d30a37da0'),
-       (5, '1l3qnaenScIWpAyJRbjLmP', 'PUZZLE', 'Sugarbowl', '03:31', 'https://p.scdn.co/mp3-preview/efe2a61023c9cebe056b51c76a28b331d864da33?cid=85cc36f6ed374905aebe0d7e5ea27763', 'https://i.scdn.co/image/ab67616d0000b2731ad157781a18ed1c8e832670'),       (5, '72p6J6txwmawWVC9Gc9vwg', '오늘밤', 'Sugarbowl', '3:30', 'https://p.scdn.co/mp3-preview/5c23ba90529f246c9137299da5bce2dde89a834a?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (5, '1l3qnaenScIWpAyJRbjLmP', 'PUZZLE', 'Sugarbowl', '03:31',
+        'https://p.scdn.co/mp3-preview/efe2a61023c9cebe056b51c76a28b331d864da33?cid=85cc36f6ed374905aebe0d7e5ea27763',
+        'https://i.scdn.co/image/ab67616d0000b2731ad157781a18ed1c8e832670'),
+       (5, '72p6J6txwmawWVC9Gc9vwg', '오늘밤', 'Sugarbowl', '3:30',
+        'https://p.scdn.co/mp3-preview/5c23ba90529f246c9137299da5bce2dde89a834a?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b2736610b1cd859feae3598e7d22'),
-       (4, '33xRp6ZX1DKraRFHR9ZDck', 'Forest', 'Choi Yu Ree', '03:48', 'https://p.scdn.co/mp3-preview/cfecfbfac21935ed9ff395b1cc12debad3b2a50d?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (4, '33xRp6ZX1DKraRFHR9ZDck', 'Forest', 'Choi Yu Ree', '03:48',
+        'https://p.scdn.co/mp3-preview/cfecfbfac21935ed9ff395b1cc12debad3b2a50d?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b2736012b2482accbd98980ad1c5'),
-       (5, '12n9WBfLII5EdQFzQiQFqv', '"Wish', 'Choi Yu Ree', '03:57', 'https://p.scdn.co/mp3-preview/23440e95aea744bc060c0badc4e493d322f9ef4f?cid=85cc36f6ed374905aebe0d7e5ea27763',
+       (5, '12n9WBfLII5EdQFzQiQFqv', '"Wish', 'Choi Yu Ree', '03:57',
+        'https://p.scdn.co/mp3-preview/23440e95aea744bc060c0badc4e493d322f9ef4f?cid=85cc36f6ed374905aebe0d7e5ea27763',
         'https://i.scdn.co/image/ab67616d0000b27316f90eca8734936f3f8ef7dc');
 
 
